221350c720344372d5d6cd9b916ac68de1d99f83
AaltonenSan <santtu.36@gmail.com>
2023-01-26 12:59:13 +0200

    step2 - step6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     id: 1,
@@ -26,6 +28,57 @@ let persons = [
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+  if (persons.find(person => person.name === body.name)) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 1000) + 1,
+    name: body.name,
+    number: body.number
+  }
+  console.log(person)
+
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+app.get('/info', (req, res) => {
+  const count = persons.length
+  const date = new Date()
+  res.write(`<p>Phonebook has info for ${count} people<p>`)
+  res.write(`<p>${date}<p>`)
+  res.end()
 })
 
 const PORT = 3001
