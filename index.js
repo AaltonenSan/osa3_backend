6959a59fc8fc1b7c4acb9921d0ab8ade50a346d7
AaltonenSan <santtu.36@gmail.com>
2023-02-21 23:24:54 +0200

    3.15-3.18 all endpoints use db & added express error handling

--- index.js
+++ index.js
@@ -6,48 +6,23 @@ const cors = require('cors')
 const Person = require('./models/Person')
 const { response } = require('express')
 
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
 }
 
 morgan.token('content', function getContent(req) {
   return JSON.stringify(req.body)
 })
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time :content', {
   skip: function (req, res) { return req.method !== 'POST' }
 }))
 app.use(morgan('tiny', {
   skip: function (req, res) { return req.method === 'POST' }
 }))
-
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-532523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6423122"
-  }
-]
 
 // Get all contacts
 app.get('/api/persons', (req, res) => {
@@ -57,7 +32,7 @@ app.get('/api/persons', (req, res) => {
 })
 
 // Get one contact by Id
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then(person => {
       if (person) {
@@ -66,18 +41,16 @@ app.get('/api/persons/:id', (req, res) => {
         res.status(404).end()
       }
     })
-    .catch(error => {
-      console.log(error)
-      res.status(500).end()
-    })
+    .catch(error => next(error))
 })
 
 // Delete one contact by Id
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // Add new contact
@@ -89,11 +62,6 @@ app.post('/api/persons', (req, res) => {
       error: 'name or number missing'
     })
   }
-  // if (Person.find({ name: body.name })) {
-  //   return res.status(400).json({
-  //     error: 'name must be unique'
-  //   })
-  // }
 
   const person = new Person({
     name: body.name,
@@ -105,12 +73,40 @@ app.post('/api/persons', (req, res) => {
   })
 })
 
-app.get('/info', (req, res) => {
-  const count = persons.length
+// Update contact
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+// Info page
+app.get('/info', (req, res, next) => {
   const date = new Date()
-  res.write(`<p>Phonebook has info for ${count} people<p>`)
-  res.write(`<p>${date}<p>`)
-  res.end()
+  Person.countDocuments({}, function (error, count) {
+    if (error) {
+      next(error)
+    } else {
+      res.write(`<p>Phonebook has info for ${count} people<p>`)
+      res.write(`<p>${date}<p>`)
+      res.end()
+    }
+  })
 })
 
 app.use(unknownEndpoint)
@@ -119,3 +115,15 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
